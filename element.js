679ab6b580a2b3b96bf57ff7a2158fa5b04c556d
Aaron Shafovaloff <aaronshaf@gmail.com>
2017-01-16 19:52:15 -0700

    Diff improvements

--- element.js
+++ element.js
@@ -2,31 +2,24 @@ import createElementClass from 'create-element-class'
 import leven from 'leven'
 import diff from 'fast-diff'
 
-const lineTypes = {
-  ' ': 'context',
-  '+': 'addition',
-  '-': 'removal'
-}
+const lineTypes = { ' ': 'context', '+': 'addition', '-': 'removal' }
 
-const spanTypes = {
-  ' ': 'span',
-  '+': 'ins',
-  '-': 'del'
-}
+const spanTypes = { ' ': 'span', '+': 'ins', '-': 'del' }
 
 export default createElementClass({
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (this.rendered) { this.updateRendering() }
+  attributeChangedCallback (name, oldValue, newValue) {
+    if (this.rendered) {
+      this.updateRendering()
+    }
   },
-
-  connectedCallback() {
+  connectedCallback () {
     const pre = this.querySelector('pre')
     pre.style.display = 'none'
     const patch = pre.textContent
 
     this.container = document.createElement('div')
     if (document.body.attachShadow) {
-      this.container = this.container.attachShadow({mode: 'open'})
+      this.container = this.container.attachShadow({ mode: 'open' })
     }
 
     let oops = 0
@@ -36,16 +29,25 @@ export default createElementClass({
     const chunks = toChunks(lines)
 
     for (let chunk of chunks) {
-      chunk.lines.forEach((line, index, lines) => {
-        if (line.type === 'removal') {
-          // if next line is proximate replacement, show char-level diff
-          const l = leven(line.text, lines[index + 1].text)
-          if (l < 10) {
-            line.isHidden = true
-            // char-leve diff
-            lines[index + 1].diff = diff(line.text, lines[index + 1].text)
+      const removalLines = chunk.lines.filter(line => line.type === 'removal')
+      const additionLines = chunk.lines.filter(
+        line => line.type === 'addition'
+      )
+      // console.debug(chunk.lines)
+      removalLines.forEach((removalLine, index) => {
+        additionLines.forEach((additionLine, index) => {
+          if (additionLine.removalIdentified) {
+            return
           }
-        }
+          // if line is proximate replacement, show char-level diff
+          const l = leven(removalLine.text, additionLine.text)
+          if (l < 10) {
+            removalLine.isHidden = true
+            // char-level diff
+            additionLine.diff = diff(removalLine.text, additionLine.text)
+            additionLine.removalIdentified = true
+          }
+        })
       })
     }
 
@@ -53,13 +55,15 @@ export default createElementClass({
       const chunkDiv = document.createElement('div')
       chunkDiv.className = 'chunk'
       for (let line of chunk.lines) {
-        if (line.isHidden) continue
+        if (line.isHidden) {
+          continue
+        }
         const blockDiv = document.createElement('div')
         blockDiv.className = 'line'
 
         let inlineElements = []
         if (line.diff) {
-          line.diff.forEach((diffSection) => {
+          line.diff.forEach(diffSection => {
             let element
             if (diffSection[0] === diff.INSERT) {
               element = document.createElement('ins')
@@ -86,7 +90,7 @@ export default createElementClass({
           inlineElements.push(document.createElement('span'))
           inlineElements[0].textContent = line.text
         }
-        inlineElements.forEach((inlineElement) => {
+        inlineElements.forEach(inlineElement => {
           blockDiv.appendChild(inlineElement)
         })
         chunkDiv.appendChild(blockDiv)
@@ -102,10 +106,8 @@ export default createElementClass({
     }
 
     this.appendChild(this.container)
-
   },
-
-  updateRendering() {
+  updateRendering () {
     this.rendered = true
   }
 })
@@ -119,24 +121,29 @@ function isChunkHeader (str) {
 }
 
 function findFirstChunkIndentation (string) {
-  return string.split('\n').reduce((prev, line) => {
-    if (prev !== null) return prev
-    if (line.substr(0, 2) === '@@') {
-      return 0
-    }
-    const atChar = line.search(/(\s)@@/)
-    if (atChar !== -1) {
-      return line.indexOf('@@')
-    }
-    return null
-  }, null)
+  return string.split('\n').reduce(
+    (prev, line) => {
+      if (prev !== null) {
+        return prev
+      }
+      if (line.substr(0, 2) === '@@') {
+        return 0
+      }
+      const atChar = line.search(/(\s)@@/)
+      if (atChar !== -1) {
+        return line.indexOf('@@')
+      }
+      return null
+    },
+    null
+  )
 }
 
 // normalize the chunk if it was indented in the <pre> element
 function removeIndentation (patch) {
-  const lines = patch.split("\n")
+  const lines = patch.split('\n')
   const indentation = findFirstChunkIndentation(patch)
-  return lines.map((line) => {
+  return lines.map(line => {
     return line.substr(indentation)
   })
 }
@@ -144,21 +151,19 @@ function removeIndentation (patch) {
 // remove all lines up until first chunk
 // TODO: don't mutate
 function removeBeginningNonChunkLines (lines) {
-  while (lines.length && !isChunkHeader(lines[0])) { lines.shift() }
+  while (lines.length && !isChunkHeader(lines[0])) {
+    lines.shift()
+  }
 }
 
 function toChunks (lines) {
   const chunks = []
   for (let line of lines) {
     if (isChunkHeader(line)) {
-      chunks.push({
-        lines: []
-      })
+      chunks.push({ lines: [] })
     } else if (lineTypes[line[0]]) {
-      chunks[chunks.length - 1].lines.push({
-        type: lineTypes[line[0]],
-        text: line.substr(1)
-      })
+      chunks[chunks.length -
+        1].lines.push({ type: lineTypes[line[0]], text: line.substr(1) })
     }
   }
   return chunks
